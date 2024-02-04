0eb81e9fc9afe0102dddefc1aa014cb24c016e0b
SanctuaryOfProjects <dzhumabayevakarina@gmail.com>
2024-02-04 21:36:04 +0600

    test 7

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,13 +26,11 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_COOKIE_SECURE = False 
-
 ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Application definition
 
