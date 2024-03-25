9d313ce6af4cd7ea4a1ccaefa8b19f3775f71aab
SanctuaryOfProjects <dzhumabayevakarina@gmail.com>
2024-03-25 16:00:20 +0500

    -111

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/lib/postgresql/data/'
+RAILWAY_VOLUME_MOUNT_PATH = '/var/lib/postgresql/data'
+MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
